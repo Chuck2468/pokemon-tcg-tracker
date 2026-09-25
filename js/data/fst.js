4c5dc504df33213270a0a940d19abf481c492874
Carlos <carlosjover24@gmail.com>
2026-09-25 21:40:02 +0200

    Versión 2.6.4 - Edit Mark

--- js/data/fst.js
+++ js/data/fst.js
@@ -1,3 +1,3 @@
 export default [
-  { id: "235", nombre: "Juez", tipo: "Partidario", numero: "235/264", set: "FST", reprintGroup: 30010, variantes: { normal: 0, reverse: 0, holo: 0, fullart: 0 } }
+  { id: "235", nombre: "Juez", tipo: "Partidario", numero: "235/264", set: "FST", reprintGroup: 30010, energia: "NA", variantes: { normal: 0, reverse: 0, holo: 0, fullart: 0 } }
 ];
